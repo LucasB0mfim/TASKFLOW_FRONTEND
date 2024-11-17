be5dd24e8c8ae04e98f21eaedbf4c4104cd6e113
lucas <lucasbomfimsobral@gmail.com>
2024-11-17 08:46:20 -0300

    feat(store): Adicionar o store ao projeto.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Provider } from 'react-redux';
+
+import { store } from './store';
+
 import { GlobalStyle } from "./styles";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
-    </>
+    </Provider>
   );
 }
 
