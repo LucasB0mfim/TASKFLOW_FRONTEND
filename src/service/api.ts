a0855d15ff77dee9cc5c1d3a7b6b5a660fa9f3a1
lucas <lucasbomfimsobral@gmail.com>
2024-11-17 08:44:54 -0300

    feat(api): Adicionar requisição a api.

---
+++ src/service/api.ts
@@ -0,0 +1,43 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export type Tarefa = {
+  id: number;
+  nome: string;
+  custo: number;
+  dataLimite: string;
+  ordem: number;
+}
+
+const api = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:8080/api'
+  }),
+  endpoints: (builder) => ({
+    buscarTarefas: builder.query<Tarefa[], void>({
+      query: () => 'tarefas'
+    }),
+    salvarTarefa: builder.mutation<Tarefa, Partial<Tarefa>>({
+      query: (novaTarefa) => ({
+        url: 'tarefas',
+        method: 'POST',
+        body: novaTarefa
+      })
+    }),
+    atualizarTarefa: builder.mutation<Tarefa, { tarefaId: number; tarefa: Tarefa }>({
+      query: ({ tarefaId, tarefa }) => ({
+        url: `tarefas/${tarefaId}`,
+        method: 'PUT',
+        body: tarefa,
+      })
+    }),
+    excluirTarefa: builder.mutation<void, number>({
+      query: (tarefaId) => ({
+        url: `tarefas/${tarefaId}`,
+        method: 'DELETE',
+      })
+    })
+  })
+})
+
+export const { useBuscarTarefasQuery, useSalvarTarefaMutation, useExcluirTarefaMutation, useAtualizarTarefaMutation } = api
+export default api
